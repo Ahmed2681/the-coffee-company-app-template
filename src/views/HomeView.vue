8f93fdd8f66a4a257df99b82fa90ceb7e8526344
Ahmed Şenlikoğlu <ahmed.senlikoglu@beezsoft.com>
2025-05-28 11:52:40 +0300

    initialization

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="text-2xl">{{ msg }}</h1>
+    <h3 class="text-red-500 text-6xl">
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
